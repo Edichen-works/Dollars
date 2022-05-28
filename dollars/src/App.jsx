4b9ba0b03833a4cffc72c1c34978d08845a1dea9
edichen123 <edichenjl@outlook.com>
2022-05-28 23:29:56 +0800

    added route

--- dollars/src/App.jsx
+++ dollars/src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
 import Header from "./Component/Header";
+import { Route, Routes } from "react-router";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
     <div className=" h-screen w-screen">
       <Header />
+      
     </div>
   );
 }
--- dollars/src/Component/Header.jsx
+++ dollars/src/Component/Header.jsx
@@ -3,7 +3,7 @@ import LOGO from "./IMG/Logo";
 
 const Header = () => {
   return (
-    <div className="border border-black block ml-auto mr-auto w-1/2 h-auto">
+    <div className=" block ml-auto mr-auto w-1/2 h-auto">
       <img className="h-24 w-auto ml-auto mr-auto" src={LOGO} alt="LogPhoto" />
     </div>
   );
